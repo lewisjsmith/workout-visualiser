c0c25db446b831400adbe992edd04da3bb18cbda
Lewis <sendto.lewissmith@gmail.com>
2023-10-17 11:29:14 +0100

    Preliminary localStorage added.

--- src/App.tsx
+++ src/App.tsx
@@ -4,57 +4,45 @@ import { Exercise } from "./features/exercise/Exercise"
 import "./App.css"
 import BodyFrame from "./features/bodyFrame/BodyFrame"
 
+import { useEffect } from "react"
+
 function App() {
+
+  function initLocalStorage() {
+    localStorage.setItem("test",
+      JSON.stringify([
+        {
+          id: "123",
+          package: {
+            exercise: {
+              title: "Deadlift",
+              musclesWorked: ["trapezius, glutes, abs"]
+            },
+            volume: 2
+          }
+        },
+        {
+          id: "124",
+          package: {
+            exercise: {
+              title: "Barbell Squat",
+              musclesWorked: ["glutes", "hamstrings", "lats"]
+            },
+            volume: 3
+          }
+        },
+      ]))
+  }
+
+  useEffect(() => {
+    initLocalStorage()
+  })
+
   return (
     <div className="app">
-      <h1 className="page-title">Workout Visualiser</h1>
+      <h1 className="page-title">WORKOUT VISUALISER</h1>
       <Exercise />
       <BodyFrame />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header> */}
     </div>
   )
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -3,13 +3,25 @@ import counterReducer from "../features/counter/counterSlice"
 import exerciseReducer from "../features/exercise/exerciseSlice"
 import bodyFrameReducer from "../features/bodyFrame/bodyFrameSlice"
 
+function loadLocalStorage() {
+  const testStorage = localStorage.getItem("test");
+  return JSON.parse(testStorage);
+}
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     exercise: exerciseReducer,
     bodyFrame: bodyFrameReducer
   },
+  preloadedState: {
+    exercise: {
+      exerciseList: loadLocalStorage()
+    }
+  }
 })
+
+// preloadedState
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
